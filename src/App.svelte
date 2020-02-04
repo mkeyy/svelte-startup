c08c010ff8f539d9fd77446905a348b7648da229
mickor <michal.korman@ivision.pl>
2020-02-04 13:15:41 +0100

    svelte startup with sass & inline svg

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let name;
+</script>
+
+<main>
+    <div class="ivn-container">
+        <h1>Hello {name}!</h1>
+        <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    </div>
+</main>
+
+<style lang="scss" global>
+    @import "./styles/global.scss";
+</style>
---
+++ src/components/AssetSVG.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let svgSRC;
+    export let classes;
+
+    const svg = svgSRC.replace('<svg', '<svg class="' + classes + '"');
+</script>
+
+{@html svg}
